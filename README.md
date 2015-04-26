3c6350a0a4e44574443376f531deee6f06f548d4
Nine <nikolay_ratchev@yahoo.com>
2015-04-26 13:25:52 +0300

    Unnecessary commands deleted or commented out, more comments added for clarification

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
+---
+output: html_document
+---
 # GetCleanData_CourseProject
+
+This repo is created for the Coursera's "Getting and Cleaning Data" course project. It contains two files:
+
+* an R script that creates a tidy data set (as described by the task specification);
+* a code book giving information oabout the variables and their levels. 
+
+The strategy undertaken is as follows:
+
+In **STEP 1**, the separate files are clipped together using cbind and rbind commands, to form file containing all the information from the training and the test sets. 
+
+In **STEP 2**, a subset of the STEP 1 data set is created, containing only the variables of intreset (i.e., containing measurements of mean and standard deviation). To do this, the file containing the feature names is read and a vector of indexes is created corresponding to the measurements of interest. Then a subset is created, containing the columns with the corresponding indexes, precede by "id"" and "activity". 
+
+It is assumed that mean frequencies are not needed for this subset, so an additional effort is made to exclude these from the subset. 
+
+In **STEP 3**, descriptive activity names to name the activities in the data set are given by first reading the file containing activity labels, then doing some editing (convert to lower case), and finally merging the table with the subset created in Step 2, using the common variable (label code). The duplicate column ("code") is then deleted.
+
+In **STEP 4**, measurements are given appropriate descriptive names. Some text editing is made to discard illegal characters and replace the abbreviations with more informative strings. For the ease of reading, some underscores ("_") and uppercase letters are added. The full description of the features is given in the code book. 
+
+Finally in this step the edited vector is assigned as names for the data set from Step 3. To conform with the data set structure, first the "activity" abd "id" names are assigned, followed by the vector described above. 
+
+In the final **STEP 5**, a second, tidy data set is created, with the average of each variable for each activity and each subject. To do this, first the data set is reshaped into "long" form, so that different measurements become levels of one variable ("feature"). The data are then summarized by subject, activity, and measurement using the ddply command form the plyr package [(Wickham, 2011)](http://www.jstatsoft.org/v40/i01/). The resulting data set is written to "tidyData.txt"" file. As a final step, all the abject in the R workplace but the final data set ("tidy2") are removed.
+
+Additional details are given as comments in the R script
+
+##References
+Hadley Wickham (2011). The Split-Apply-Combine Strategy for Data Analysis. Journal of Statistical Software, 40(1), 1-29. URL http://www.jstatsoft.org/v40/i01/. 
+
+
--- run_analysis.R
+++ run_analysis.R
@@ -1,145 +1,154 @@
-# STEP 0: Preliminary actions
-# download & unzip files
+
+
+# STEP 0: Preliminary actions: download & unzip files
+
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "./ProjectData.zip")
 unzip("ProjectData.zip")
 
+
 # STEP 1: Merges the training and the test sets to create one data set.
-# read files
-## read 'training' data
-x.train <- read.table("../CourseProject/UCI HAR Dataset/train/X_train.txt")
-y.train <- read.table("../CourseProject/UCI HAR Dataset/train/y_train.txt")
-s.train <- read.table("../CourseProject/UCI HAR Dataset/train/subject_train.txt")
 
-## read 'test' data
-x.test <- read.table("../CourseProject/UCI HAR Dataset/test/X_test.txt")
-y.test <- read.table("../CourseProject/UCI HAR Dataset/test/y_test.txt")
-s.test <- read.table("../CourseProject/UCI HAR Dataset/test/subject_test.txt")
+# 1.1 read files
+## 1.1.1 read 'training' data
+x.train <- read.table("UCI HAR Dataset/train/X_train.txt")
+y.train <- read.table("UCI HAR Dataset/train/y_train.txt")
+s.train <- read.table("UCI HAR Dataset/train/subject_train.txt")
 
-# clip files
-## clip training data files togehter
+## 1.1.2 read 'test' data
+x.test <- read.table("UCI HAR Dataset/test/X_test.txt")
+y.test <- read.table("UCI HAR Dataset/test/y_test.txt")
+s.test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+
+# 1.2 clip files together
+## 1.2.1 clip training data files togehter
+## the order of clipping is "subject" (s), "activity" (y), "measurements" (x)
 syx.train <- cbind(s.train, y.train, x.train)
-head(syx.train[,1:4])
-## clip test data files togehter
+
+## 1.2.2 clip test data files togehter, same order as for the training data
 syx.test <- cbind(s.test, y.test, x.test)
-head(syx.test[,1:4])
-## combine all data
+
+## 1.2.3 combine all data
 syx.all <- rbind(syx.train, syx.test)
-head(syx.all)
 
-# -------
 
-# STEP 2: Extracts only the measurements on the mean and standard deviation for each measurement.
+# STEP 2: Extracts only the measurements on the mean and standard deviation 
+# for each measurement.
 
-#reaed file with measurement labels
-features <- read.table("../CourseProject/UCI HAR Dataset/features.txt", 
-                       colClasses = "character") # convert to characther! 
-str(features)
-head(features)
+# 2.1 read file with measurement labels (a.k.a. "features")
+features <- read.table("UCI HAR Dataset/features.txt")
+# str(features)
+# head(features)
 
-# subset the columnt needed
+# 2.2 subset the column needed
 features <- features[,2]
 
-# create a variable with indexes of the variables containing only "mean" or "std"
+# 2.3 create a vector of indexes of the variables containing only "mean" or "std"
 columns <- grep("mean|std", features)
 
-# create another variable with the indexes of the variables containing "Freq"
+# 2.4 create another variable with the indexes of the variables containing "Freq"
 rm.columns <- grep("Freq", features)
 
-
-# assume we don't need meanFreq -> get rid of that indexes
+# 2.5 assume we don't need meanFreq -> get rid of that indexes
 columns_cleaned <- columns[!columns %in% rm.columns]
-length(columns_cleaned)
-length(columns)
+# length(columns_cleaned)
+# length(columns)
 
-
-# create a subset of syx.all, containing only the measurements in interest
-subset <- syx.all[, c(1,2, (columns_cleaned + 2))] # we need to add to to the indices, 
+# 2.6 create a subset of syx.all, containing only the measurements in interest
+subset <- syx.all[, c(1,2, (columns_cleaned + 2))] # we need to add 2 to the indices, 
 # beacause we have two more variables at the beginning
 # dim(subset)
 # names(subset)
 
-# STEP 3: Uses descriptive activity names to name the activities in the data set
-activities <- read.table("../CourseProject/UCI HAR Dataset/activity_labels.txt")
-activities
-# activities <- activities[,2]
+
+        # STEP 3: Uses descriptive activity names to name the activities in the data set
+
+# 3.1 read file with activity labels
+activities <- read.table("UCI HAR Dataset/activity_labels.txt")
+
+# 3.2 convert labels to lowercase
 activities[,2] <- tolower(activities[,2])
 
-
-# names(sxy.all)
-# sxy.all$activity[activities$V2]
+# 3.3. check out the names of the variables in the subset created in step 2.6
 head(subset[,1:3])
-
 names(subset)
+
+# 3.4 give activities descriptive variable names
 names(activities) <- c("code", "activity")
 
+# 3.5 merge activities & subset from step 2.6
 subset.merged <- merge(activities, subset, by.x = "code", by.y = "V1.1")
-names(subset.merged)
-head(subset.merged[,1:5])
 
-# remove unnecessary column
+# 3.6 check out results
+names(subset.merged)
+head(subset.merged[,1:5]) # "code" and "activity" are in fact duplicates
+
+# 3.7 remove duplicate column ("code")
 subset.merged2 <- subset.merged[, -1]
-names(subset.merged2)
-head(subset.merged2[1:5])
+# names(subset.merged2)
+# head(subset.merged2[1:5])
+
 
 # STEP 4: Appropriately labels the data set with descriptive variable names. 
+
+# remember "columns_cleaned" is vector containing only the indexes of the variables 
+# specified in the task description ("mean | std", "meanFred" excluded)
+# columns_cleaned
+
+# 4.1. create a vector containing the names of the measurements needed
 col.names <- features[columns_cleaned]
-length(col.names)
-
-#the names given in step 1 are not valid column names! Fix: 
-# valid_column_names <- make.names(names=names(sxy.all), unique=TRUE, allow_ = TRUE)
-# names(sxy.all) <- valid_column_names
-
+# length(col.names) # check result
+# col.names
 # class(col.names)
-col.names <- features[columns_cleaned]
-col.names
+
+# the labels extracted in step 4.1 are not valid column names 
+# 4.2 Fix labels by editing text
 cn1 <- sub("^t", "time_", col.names)
 cn2 <- sub("^f", "frequency_", cn1)
 cn3 <- gsub("-", "_", cn2)
 cn4 <- sub("\\(\\)", "", cn3)
-cn5 <- sub("Acc", "Acceleration", cn4)
+cn5 <- sub("Acc", "Acceleration", cn4) # this is the final manipulation
 
+# 4.3 assign names to the final (so far) version of the data
 names(subset.merged2) <- c("activity", "id", cn5)
-names(subset.merged2)
-View(subset.merged2)
+# names(subset.merged2)
+# View(subset.merged2)
 
 
 # STEP 5: From the data set in step 4, creates a second, independent tidy data set 
 # with the average of each variable for each activity and each subject.
 
-write.table(subset.merged2, "subset.merged2.csv", sep=",", row.names = F)
-?write.table
-table(subset.merged2$id, subset.merged2$activity)
-names(subset.merged2)
-
-
-# rehape data into long form
+# 5.1 reshape data into long form, so that different measurements become levels 
+# of the variable "feature" 
 library(reshape2)
 dataMelt <- melt(subset.merged2, id = c("id", "activity"), measure.vars = 3:68)
-head(dataMelt)
-dim(dataMelt)
+
+# 5.2 Check results from step 5.1
+# head(dataMelt)
+# dim(dataMelt)
 str(dataMelt)
+
+# 5.3 we need to coerce variable "activity" to factor 
 dataMelt$activity <- factor(dataMelt$activity)
-dataMelt$id <- factor(dataMelt$id)
-# colnames(dataMelt[,3])
-
-# dataCast <- dcast(dataMelt, activity ~ variable)
-# dataCast
-# 
-# ?ddply
-write.table(dataMelt, "dataMelt.csv", sep = ",")
 
 
-# rename "variable" into "feature" 
-
-# names(dataMelt)
+# 5.4 summarize data by subject (id), activity and feature (variable), giving the mean
+# (as is the task description) 
+library(plyr)
 tidy1 <- ddply(dataMelt, .(id, activity, variable), summarize,
-               mean = round(mean(value),2))b # this is it! 
-head(tidy1, n = 70)
-dim(tidy1)
+               mean = round(mean(value),2)) # this is it! 
+# head(tidy1, n = 70)
+# dim(tidy1)
 
-# rename "variable" to "feature" via dplyr
+# 5.5 rename "variable" to "feature" via dplyr
+library(dplyr)
 tidy2 <- rename(tidy1, feature = variable)
-head(tidy2)
 
+# check results
+# head(tidy2)
+
+# 5.6 write the final data set into a *.txt file
 write.table(tidy2, "tidyData.txt", row.name = F)
+
+# 5.7 remove all objects but "tidy2"
+rm(list=setdiff(ls(), "tidy2"))
